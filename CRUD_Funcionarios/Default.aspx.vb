ecf35bcafd73d2ebdb58a826e409ae4290b54542
Marcelo Peruzzo <marcelo.peruzzo@sponte.com.br>
2023-03-29 19:15:40 -0300

    Metodo Created feito

---
+++ CRUD_Funcionarios/Default.aspx.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Data.SqlClient
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        AddHandler btnSave.Click, AddressOf CadastrarFunc
+    End Sub
+
+
+    Protected Sub CadastrarFunc(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim connectionString As String = ConfigurationManager.ConnectionStrings("BDCrudVb").ConnectionString
+        Dim queryCreated As String = "INSERT INTO Funcionarios(Nome, Email, Endereco, Telefone) VALUES (@nome, @email, @endereco, @fone)"
+        Using connection As New SqlConnection(connectionString)
+            Dim command As New SqlCommand(queryCreated, connection)
+            command.Parameters.AddWithValue("@nome", nomeForm.Text)
+            command.Parameters.AddWithValue("@email", emailForm.Text)
+            command.Parameters.AddWithValue("@endereco", enderecoForm.Text)
+            command.Parameters.AddWithValue("@fone", foneForm.Text)
+            connection.Open()
+            command.ExecuteNonQuery()
+            connection.Close()
+        End Using
+
+    End Sub
+
+End Class
